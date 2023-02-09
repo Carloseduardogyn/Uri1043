1269953dde040287cf57e147d49be4ddc9507718
Carloseduardogyn <123594790+Carloseduardogyn@users.noreply.github.com>
2023-02-09 10:36:40 -0300

    Update Uri1043.java

--- Uri1043.java
+++ Uri1043.java
@@ -3,8 +3,26 @@ package Uri;
 public class Uri1043 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		double a, b, c, perimetro, area;
+
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		c = sc.nextDouble();
+		
+		perimetro = a + b + c;
+		area = (a + b) * c / 2;
+
+		if ((a < b + c) && (b < a + c) && (c < a + b)) {
+			System.out.printf("Perimetro = %.1f%n", perimetro);
+
+		} else
+			System.out.printf("Area = %.1f%n", area);
+
+		sc.close();
 	}
 
 }
